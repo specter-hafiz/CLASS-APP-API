bf3d2230c5536c6b0487a0a3eebaef66896e80fd
Hafiz <hafizhuaru123@gmail.com>
2025-07-30 04:03:20 +0000

    changes to audio controller

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const routeNotFound = require("./middlewares/routeNotFoundMiddleware");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "100mb" })); // Increase body size limit for large audio files
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use("/api/v1", routes);
 
